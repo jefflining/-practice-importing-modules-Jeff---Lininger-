4ccc9649731feb5ceb716fb69b73a7be9963410d
jeff <jefflining@gmail.com>
2025-04-28 17:16:32 -0500

    push 2

--- task-manager/app.js
+++ task-manager/app.js
@@ -2,4 +2,19 @@ const taskManager = require('./taskManager.js');
 const fileHandler = require('./fileHandler.js');
 const path = require('path');
 
-console.log(addTask(tasks, "Walk Dog"));
+let taskList = ['Work'];
+
+taskManager.addTask(taskList, 'Walk dog');
+taskManager.addTask(taskList, 'Clean room');
+taskManager.addTask(taskList, 'Do dishes');
+taskManager.listTasks(taskList);
+
+let fileTasks = fileHandler.loadTasks('./tasks.json');
+
+for (let task of fileTasks) {
+    if (task.length > 0  && !taskList.includes(task)) {
+       taskList.push(task);
+    }
+}
+
+console.log(taskList);
